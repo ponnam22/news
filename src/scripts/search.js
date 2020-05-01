311db75c7484be3661b8877eec2b89f426b1fe98
arbaaj19 <60877294+arbaaj19@users.noreply.github.com>
2020-05-01 20:08:39 +0530

    test7

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -38,18 +38,13 @@ let makeSomeHTML = (response) => {
 let select = document.querySelector('.not-found');
          if (obj.articles.length < 1){
           //select.innerHTML = '<h1>No article was found based on the search.</h1>';    
-             alert("keyword not match found");
+             alert("No article was found");
            
              var h1 = document.createElement("h1");
              h1.className = "not-found";
-             h1.innerHTML = "No article was found based on the search.";
+             h1.innerHTML = "";
             select.appendChild(h1);
          }
- 
-         
-          
-      
-
 
 
  document.querySelector('#news-articles').innerHTML = output;
