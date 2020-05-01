fa94af600dbe0fefdf4c8e725837cf4cee973f91
arbaaj19 <60877294+arbaaj19@users.noreply.github.com>
2020-05-01 13:07:30 +0530

    error

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,6 +1,6 @@
 const searchFrom = document.querySelector(".form-search");
-const input = document.getElementById("#search");
-const select =document.querySelector('#selector');
+const input = document.getElementById("search");
+
 searchFrom.addEventListener('submit',retrieve)
  
 
@@ -8,17 +8,6 @@ function retrieve(e){
   e.preventDefault() 
   let topic=input.value;
        let url =   `https://newsapi.org/v2/everything?q=${topic}&apiKey=8857ddfa72ae47238500738f55cc70f1`
-    
-       let nodeCreated = `
-     <li class="article">
-           <img class="article-img" src="IMG-20180709-WA0015.jpg" alt="image" style="width:100%">
-           <h2 class="article-title"> hello hii</h2>
-           <p class="article-description">hxjhsgyghwyugyuvyux6fwxgfw</p>
-           <span class="article-author" style="display: block;"> shaikh.a.a</span>
-           <a class="article-link" href="google.com"> hhjh</a>
-       </li>
-
-     `;
 
      let output = "";
 
@@ -46,7 +35,14 @@ let makeSomeHTML = (response) => {
      output = output+ outTemplate; 
    
  }
+
+let select =document.querySelector('.selector');
+         if (obj.totalResults == 0){
+             alert("keyword not match found");
+           select.innerHTML= `<h1>data is not found try different keyword</h1>`;
+         }
  
+
  document.querySelector('#news-articles').innerHTML = output;
 }
 httpGetAsync(url, makeSomeHTML);
